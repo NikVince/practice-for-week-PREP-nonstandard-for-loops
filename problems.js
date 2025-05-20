e28f252f34fae15b0f616dc36a7103e1187deb6e
NikVince <165183152+NikVince@users.noreply.github.com>
2025-05-20 22:01:38 +0200

    UPDATED problems.js with logic that passes tests for all remaining functions.

--- problems.js
+++ problems.js
@@ -35,23 +35,69 @@ function oddReverse(arr) {
 function secondPower(arr) {
     // Return an array containing all indices that are powers of 2
     // Your code here
+    if (!Array.isArray(arr)) {
+        return [];
+    }
+
+    let result = [];
+
+    for (let i = 1; i < arr.length; i = i * 2) {
+        result.push(arr[i]);
+    }
+
+    return result;
 }
 
 function nthPower(arr, n) {
     // Return an array containing all indices that are powers of n
     // Your code here
+    if (!Array.isArray(arr)) {
+        return [];
+    }
+
+    let result = [];
+
+    for (let i = 1; i < arr.length; i = i * n) {
+        result.push(arr[i]);
+    }
+
+    return result;
 }
 
 function firstHalf(arr) {
     // Return an array containing the first half of an array
     // Include middle index on odd length arr
     // Your code here
+       if (!Array.isArray(arr)) {
+        return [];
+    }
+
+    let result = [];
+    let half = arr.length / 2;
+
+    for (let i = 0; i < half; i++) {
+        result.push(arr[i]);
+    }
+
+    return result;
 }
 
 function secondHalf(arr) {
     // Return an array containing the second half of an array
     // Exclude middle index on odd length arr
     // Your code here
+    if (!Array.isArray(arr)) {
+        return [];
+    }
+
+    let result = [];
+    let half = Math.ceil(arr.length / 2); // Use Math.ceil to round up
+
+    for (let i = half; i < arr.length; i++) {
+        result.push(arr[i]);
+    }
+
+    return result;
 }
 
 module.exports = {
